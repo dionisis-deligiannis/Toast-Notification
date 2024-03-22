f4e68e40a8ce139082d039a0795f4b65d9561250
dionisis-deligiannis <delisdionysios@gmail.com>
2024-03-22 18:58:25 +0200

    setup icons

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 let toastBox = document.getElementById("toastBox");
+let successMsg =' <i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = ' <i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
+let invalidMsg = ' <i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
 
-function showToast(){
+function showToast(msg){
     let toast = document.createElement("div");
     toast.classList.add("toast");
-    toast.innerHTML = 'success';
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
 }
